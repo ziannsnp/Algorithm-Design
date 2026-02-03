b4c3beee2c133e1d03578bccb1a528f966ed9c33
ziannsnp <zianporrutai@gmail.com>
2026-02-03 19:14:10 +0700

    try to push

---
+++ Brute Force/Gen Barcode/gen_barcode.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void combi_exact(int n,
+                 vector<int> &sol,
+                 int len,
+                 int k, int chosen)
+{
+    if (len < n)
+    {
+        if (len - chosen < n - k)
+        {
+            sol[len] = 0;
+            combi_exact(n, sol, len + 1, k, chosen);
+        }
+        if (chosen < k)
+        {
+            sol[len] = 1;
+            combi_exact(n, sol, len + 1, k, chosen + 1);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+            if (sol[i] == 1){
+                cout << 1 ;
+            } else {
+                cout << 0 ;
+            }
+                
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    return 0;
+}
